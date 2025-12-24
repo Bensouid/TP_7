ab07b9f8320d2e45995163a0ac8723f659fd1c7c
Bensouid <hamzabensouid@gmil.com>
2025-12-24 12:58:05 +0100

    first commit

---
+++ src/package_com/example/EX2/FileItem.java
@@ -0,0 +1,15 @@
+package package_com.example.EX2;
+
+public class FileItem extends FsItem {
+    private long size; // en octets
+
+    public FileItem(String name, long size) {
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public long getSize() {
+        return size;
+    }
+}
